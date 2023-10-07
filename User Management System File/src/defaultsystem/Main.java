9de6f31d44c5fcc7f90a482d843e29732e12b0c0
NguyenBaDat20 <boyprocopodepgai@gmail.com>
2023-10-07 14:07:07 +0700

    FirstCommit

---
+++ User Management System File/src/defaultsystem/Main.java
@@ -0,0 +1,23 @@
+package defaultsystem;
+
+import controller.Manager;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.createNewAccount();
+                    break;
+                case 2:
+                    Manager.loginSystem();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
